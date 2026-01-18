bcf7efbc30c7308bc2cd962a5958553d55d412c8
AshaS591 <ashas17205@gmail.com>
2026-01-18 15:37:44 +0530

    Check whether a string is a palindrome

--- day2.py
+++ day2.py
@@ -4,7 +4,12 @@
 string = 'career'
 print(f"Reverse of {string} :{string[::-1]}")
 
-
+#12. Check whether a string is a palindrome.
+string = 'WOW'
+if string == string[::-1]:
+    print("Palindrome..")
+else:
+    print("Not a palindrome..")
 
 
 
