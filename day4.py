f768b59b7e2255e80beac3d08885bf6723e035c5
AshaS591 <as1579119@gmail.com>
2026-01-29 13:45:02 +0530

    Count digits

--- day4.py
+++ day4.py
@@ -6,7 +6,7 @@ while num!=0:
     digit = num%10
     rev = rev*10+digit
     num //= 10
-print(f'Reverse of {org} = {rev}')
+print(f'Reverse of {org} is {rev}')
 
 #2. Check palindrome number
 num = int(input("Enter a number :"))
@@ -20,6 +20,13 @@ if rev == org:
     print(f'{org} is a palindrome')
 
 #3. Count digits
+num = int(input("Enter a number :"))
+numbers = str(num)
+digits = 0
+for char in numbers:
+    if char in "0123456789":
+        digits += 1
+print(f"Number of digits in {num} are {digits}")
 
 
 #4. Sum of digits
