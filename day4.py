7ebf257ed940b6154387f8fcc68f244e0f8625b9
AshaS591 <as1579119@gmail.com>
2026-01-29 13:40:29 +0530

    Reverse a number

---
+++ day4.py
@@ -0,0 +1,18 @@
+#1. Reverse a number
+num = int(input("Enter a number :"))
+org = num
+rev = 0
+while num!=0:
+    digit = num%10
+    rev = rev*10+digit
+    num //= 10
+print(f'Reverse of {org} = {rev}')
+
+#2. Check palindrome number
+
+#3. Count digits
+
+
+#4. Sum of digits
+
+#5. Armstrong number
