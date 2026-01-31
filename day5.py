00cb8e1ebfe064bacc02e0abecb69a7170d276b5
AshaS591 <ashas17205@gmail.com>
2026-01-31 08:20:42 +0530

    GCD of two numbers

--- day5.py
+++ day5.py
@@ -1,10 +1,16 @@
-
 #1. Multiplication table
 number = int(input("Enter a number :"))
 for num in range(1,11):
     print(f"{number} x {num} = {number*num}")
 
 #2. GCD of two numbers
+def gcd(num1,num2):
+    if num2 == 0:
+        return num1
+    else:
+        return gcd(num2, num1%num2)
+num1,num2 =20,0
+print(f"GCD of {num1}, {num2} = {gcd(num1,num2)}")
 
 #3. LCM of two numbers
 
