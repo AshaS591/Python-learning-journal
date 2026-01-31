54d43f1508fbdf75b91a23b9afb505758baddfb7
AshaS591 <as1579119@gmail.com>
2026-01-31 07:59:47 +0530

    Multiplication table

---
+++ day5.py
@@ -0,0 +1,19 @@
+
+#1. Multiplication table
+number = int(input("Enter a number :"))
+for num in range(1,11):
+    print(f"{number} x {num} = {number*num}")
+
+#2. GCD of two numbers
+
+#3. LCM of two numbers
+
+#4. Power of a number
+
+#5.Pattern:
+"""
+*
+**
+***
+****
+"""
